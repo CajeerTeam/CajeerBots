5c9761e21578c4c9e3eb456740d43e208c179fa3
SkiF4er <cajeerskif4er@gmail.com>
2026-04-29 02:14:32 +0300

    Add files via upload

--- bots/vkontakte/bot/main.py
+++ bots/vkontakte/bot/main.py
@@ -1,7 +1,7 @@
 from cajeer_bots.cli import main as _main
 
 def main() -> int:
-    return _main(["run", "telegram"])
+    return _main(["run", "vkontakte"])
 
 if __name__ == "__main__":
     raise SystemExit(main())
